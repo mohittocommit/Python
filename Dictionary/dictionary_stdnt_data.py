deb0b6cb9bb9c42bad07faf0d433a56d89baf940
mohittocommit <48650170+mohittocommit@users.noreply.github.com>
2021-10-30 01:32:00 +0530

    Add files via upload

---
+++ Dictionary/dictionary_stdnt_data.py
@@ -0,0 +1,12 @@
+n=int(int(input("Enter n: "))) 
+d={}
+
+for i in range(n): 
+    roll_no=int(input("Enter roll no: ")) 
+    name=input("Enter name: ") 
+    marks=int(input("Enter marks: "))
+    #adding the name and marks in dictionary data type in the refrence of roll number
+    d[roll_no]=[name,marks] 
+
+#print the complete dictionary data stored above 
+print(d.items())
